d8065e7983e3c6207bce54add7bf6ca8d581d600
madrev <maddierevill@gmail.com>
2017-01-09 14:22:45 -0800

    Add Root component to entry point

--- BenchBnB/frontend/bench_bnb.jsx
+++ BenchBnB/frontend/bench_bnb.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionApiUtil from "./util/session_api_util";
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-    window.store = configureStore();
+    const store = configureStore();
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+    ReactDOM.render(<Root store= { store }/>, root);
 });
